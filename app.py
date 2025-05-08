ac0d186905f29e70d8936f330c7d44ea6ff2fc8a
root <root@ip-172-31-90-193.ec2.internal>
2025-05-08 14:43:52 +0000

    Updated Welcome page

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-        return "Hello this is my first project in Designed and implemented an automated CI/CD pipeline using Jenkins and Docker on AWS EC2 to deploy a Python-based web application. Achieved automated container build and deployment from GitHub source changes, demonstrating real-world DevOps practices in cloud infrastructure!"
+        return "Hello this is my first project in DevOps"
 
     if __name__ == '__main__':
             app.run(host='0.0.0.0', port=5000)
